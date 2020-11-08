ae4710bfdc0018b8e43fe7a19b3bd1d71afaa30d
Charlotte000 <ramil.mifteev@yandex.ru>
2020-11-08 15:48:22 +0300

    Dividing and more test

--- Task5/5.1/Source.c
+++ Task5/5.1/Source.c
@@ -3,6 +3,83 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+bool calculate(char* string, int* result)
+{
+	Stack* stack = createStack();
+	for (size_t i = 0; i < strlen(string); i++)
+	{
+		char character = string[i];
+		if (isdigit(character))
+		{
+			// Add digit to stack
+			push(stack, atoi(&character));
+		}
+		else if (character != ' ')
+		{
+			// Get two previous items 
+			if (isEmpty(stack))
+			{
+				break;
+			}
+			int value2 = pop(stack);
+			if (isEmpty(stack))
+			{
+				break;
+			}
+			int value1 = pop(stack);
+
+			// Do math
+			switch (character)
+			{
+				case '+':
+				{
+					push(stack, value1 + value2);
+					break;
+				}
+				case '-':
+				{
+					push(stack, value1 - value2);
+					break;
+				}
+				case '*':
+				{
+					push(stack, value1 * value2);
+					break;
+				}
+				case '/':
+				{
+					push(stack, value1 / value2);
+					break;
+				}
+			}
+		}
+	}
+
+	if (isEmpty(stack))
+	{
+		deleteStack(&stack);
+		return false;
+	}
+	else
+	{
+		int answer = pop(stack);
+		if (isEmpty(stack))
+		{
+			*result = answer;
+			deleteStack(&stack);
+			return true;
+		}
+		else
+		{
+			deleteStack(&stack);
+			return false;
+		}
+	}
+
+}
 
 bool test(void)
 {
@@ -28,6 +105,11 @@ bool test(void)
 	value1 = pop(stack);
 	isPassed = isPassed && (value1 * value2 == 21);
 
+	int answer = 0;
+	isPassed = isPassed && calculate("1 5 +", &answer) && answer == 6;
+	isPassed = isPassed && calculate("1 2 + 7 2 - *", &answer) && answer == 15;
+	isPassed = isPassed && !calculate("1 + 5", &answer);
+
 	deleteStack(&stack);
 	return isPassed;
 }
@@ -40,83 +122,21 @@ int main()
 		return;
 	}
 
-	// Create stack
-	Stack* stack = createStack();
-
-	printf(">> ");
-	while (true)
+	int answer = 0;
+	char* string = malloc(sizeof(char) * 100);
+	if (string == NULL)
 	{
-		// Scan character
-		char character = ' ';
-		scanf("%c", &character);
-		if (character == '\n')
-		{
-			break;
-		}
-
-		if (isdigit(character))
-		{
-			// Add digit to stack
-			push(stack, atoi(&character));
-		}
-		else if (character != ' ')
-		{
-			// Get two previous items 
-			if (isEmpty(stack))
-			{
-				break;
-			}
-			int value2 = pop(stack);
-			if (isEmpty(stack))
-			{
-				break;
-			}
-			int value1 = pop(stack);
-
-			// Do math
-			switch (character)
-			{
-			case '+':
-			{
-				push(stack, value1 + value2);
-				break;
-			}
-			case '-':
-			{
-				push(stack, value1 - value2);
-				break;
-			}
-			case '*':
-			{
-				push(stack, value1 * value2);
-				break;
-			}
-			case '/':
-			{
-				push(stack, value1 / value2);
-				break;
-			}
-			}
-		}
+		return;
 	}
-
-	// Print the result
-	if (isEmpty(stack))
+	printf(">> ");
+	scanf("%[^\n]s", string);
+	if (calculate(string, &answer))
 	{
-		printf("Error\n");
+		printf("%d ", answer);
 	}
 	else
 	{
-		int result = pop(stack);
-		if (isEmpty(stack))
-		{
-			printf("%d\n", result);
-		}
-		else
-		{
-			printf("Error\n");
-		}
+		printf("Error");
 	}
-
-	deleteStack(&stack);
+	free(string);
 }

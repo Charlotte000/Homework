32606f1254decccf294d62887214461dce12f87b
Charlotte000 <ramil.mifteev@yandex.ru>
2021-02-25 21:01:33 +0300

    variable number length

--- semester2/Task2/Task2.1/LzwAlgorithm.cs
+++ semester2/Task2/Task2.1/LzwAlgorithm.cs
@@ -20,7 +20,7 @@ namespace Task2._1
                 {
                     var head = new Trie();
                     Trie cursor = head;
-                    int index = 0;
+                    int index = 1;
                     for (int charIndex = 0; charIndex < fileRead.Length; charIndex++)
                     {
                         byte valueByte = (byte)fileRead.ReadByte();
@@ -43,6 +43,7 @@ namespace Task2._1
                             }
                         }
                     }
+                    fileWrite.Write(BitConverter.GetBytes(index));
                 }
             }
         }
@@ -60,18 +61,27 @@ namespace Task2._1
             {
                 using (FileStream fileWrite = File.OpenWrite(uncompressedPath))
                 {
-                    dictionary = new byte[fileRead.Length / 5][];
+                    fileRead.Seek(-4, SeekOrigin.End);
+                    byte[] sizeByte = new byte[4];
+                    sizeByte[0] = (byte)fileRead.ReadByte();
+                    sizeByte[1] = (byte)fileRead.ReadByte();
+                    sizeByte[2] = (byte)fileRead.ReadByte();
+                    sizeByte[3] = (byte)fileRead.ReadByte();
+                    int size = BitConverter.ToInt32(sizeByte);
+                    fileRead.Seek(0, SeekOrigin.Begin);
+                    dictionary = new byte[size][];
                     byte[] valueBytes = new byte[4];
-                    for (int i = 0; i < fileRead.Length / 5; i++)
+                    for (int i = 0; i < dictionary.GetLength(0); i++)
                     {
-                        valueBytes[0] = (byte)fileRead.ReadByte();
-                        valueBytes[1] = (byte)fileRead.ReadByte();
-                        valueBytes[2] = (byte)fileRead.ReadByte();
-                        valueBytes[3] = (byte)fileRead.ReadByte();
+                        int bytesLength = fileRead.ReadByte();
+                        for (int j = 0; j < 4; j++)
+                        {
+                            valueBytes[j] = j < bytesLength ? (byte)fileRead.ReadByte() : (byte)0;
+                        }
                         int value = BitConverter.ToInt32(valueBytes);
                         byte character = (byte)fileRead.ReadByte();
 
-                        if (value == -1)
+                        if (value == 0)
                         {
                             dictionary[i] = new byte[1];
                             dictionary[i][0] = character;
@@ -79,10 +89,10 @@ namespace Task2._1
                         }
                         else
                         {
-                            dictionary[i] = new byte[dictionary[value].Length + 1];
+                            dictionary[i] = new byte[dictionary[value - 1].Length + 1];
                             for (int j = 0; j < dictionary[i].Length; j++)
                             {
-                                dictionary[i][j] = j != dictionary[i].Length - 1 ? dictionary[value][j] : character;
+                                dictionary[i][j] = j != dictionary[i].Length - 1 ? dictionary[value - 1][j] : character;
                                 fileWrite.WriteByte(dictionary[i][j]);
                             }
                         }
@@ -93,13 +103,28 @@ namespace Task2._1
 
         static private void WriteTrie(Trie cursor, byte valueByte, FileStream file)
         {
-            int writeValue = cursor.HasParent() ? cursor.value : -1;
-            byte[] bytes = BitConverter.GetBytes(writeValue);
-            foreach (var b in bytes)
-            {
-                file.WriteByte(b);
-            }
+            int writeValue = cursor.HasParent() ? cursor.value : 0;
+            // byte[] bytes = BitConverter.GetBytes(writeValue);
+            byte[] bytes = GetBytes(writeValue);
+            file.WriteByte(BitConverter.GetBytes(bytes.Length)[0]);
+            file.Write(bytes);
             file.WriteByte(valueByte);
+        }
+
+        static private byte[] GetBytes(int value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            int length = 0;
+            for (int i = 3; i >= 0; i--)
+            {
+                if (bytes[i] != 0)
+                {
+                    length = i + 1;
+                    break;
+                }
+            }
+            Array.Resize(ref bytes, length);
+            return bytes;
         }
     }
 }

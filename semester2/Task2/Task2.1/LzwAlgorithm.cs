ffb9201e66de76d2dbe4e59ebbc70401e840b675
Charlotte000 <ramil.mifteev@yandex.ru>
2021-02-27 18:54:51 +0300

    make trie more independent

--- semester2/Task2/Task2.1/LzwAlgorithm.cs
+++ semester2/Task2/Task2.1/LzwAlgorithm.cs
@@ -13,116 +13,74 @@ namespace Task2._1
             {
                 File.Delete(compressedPath);
             }
-            
-            using (FileStream fileRead = File.OpenRead(path))
+
+            using FileStream fileRead = File.OpenRead(path);
+            using FileStream fileWrite = File.OpenWrite(compressedPath);
+            Trie trie = new Trie();
+            for (int charIndex = 0; charIndex < fileRead.Length; charIndex++)
             {
-                using FileStream fileWrite = File.OpenWrite(compressedPath);
+                byte valueByte = (byte)fileRead.ReadByte();
+                if (charIndex == fileRead.Length - 1)
                 {
-                    var head = new Trie();
-                    Trie cursor = head;
-                    for (int charIndex = 0; charIndex < fileRead.Length; charIndex++)
+                    trie.SaveTrieElement(valueByte, fileWrite);
+                }
+                else
+                {
+                    if (trie.TryAdd(valueByte))
                     {
-                        byte valueByte = (byte)fileRead.ReadByte();
-                        if (charIndex == fileRead.Length - 1)
-                        {
-                            WriteTrie(cursor, valueByte, fileWrite);
-                        }
-                        else
-                        {
-                            if (cursor.HasChild(valueByte))
-                            {
-                                cursor = cursor.GetChild(valueByte);
-                            }
-                            else
-                            {
-                                cursor.AddChild(valueByte);
-                                WriteTrie(cursor, valueByte, fileWrite);
-                                cursor = head;
-                            }
-                        }
+                        trie.SaveTrieElement(valueByte, fileWrite);
+                        trie.ResetCursor();
                     }
-                    fileWrite.Write(BitConverter.GetBytes(Trie.counter));
                 }
             }
+            trie.SaveTrieCount(fileWrite);      
         }
 
         static public void Decompress(string path)
         {
-            byte[][] dictionary;
-
             string uncompressedPath = path.Remove(path.Length - 7);
             if (File.Exists(uncompressedPath))
             {
                 File.Delete(uncompressedPath);
             }
-            using (FileStream fileRead = File.OpenRead(path))
-            {
-                using (FileStream fileWrite = File.OpenWrite(uncompressedPath))
-                {
-                    fileRead.Seek(-4, SeekOrigin.End);
-                    byte[] sizeByte = new byte[4];
-                    sizeByte[0] = (byte)fileRead.ReadByte();
-                    sizeByte[1] = (byte)fileRead.ReadByte();
-                    sizeByte[2] = (byte)fileRead.ReadByte();
-                    sizeByte[3] = (byte)fileRead.ReadByte();
-                    int size = BitConverter.ToInt32(sizeByte);
-                    fileRead.Seek(0, SeekOrigin.Begin);
-                    dictionary = new byte[size][];
-                    byte[] valueBytes = new byte[4];
-                    for (int i = 0; i < dictionary.GetLength(0); i++)
-                    {
-                        int bytesLength = fileRead.ReadByte();
-                        for (int j = 0; j < 4; j++)
-                        {
-                            valueBytes[j] = j < bytesLength ? (byte)fileRead.ReadByte() : (byte)0;
-                        }
-                        int value = BitConverter.ToInt32(valueBytes);
-                        byte character = (byte)fileRead.ReadByte();
+            using FileStream fileRead = File.OpenRead(path);
+            using FileStream fileWrite = File.OpenWrite(uncompressedPath);
+            fileRead.Seek(-4, SeekOrigin.End);
+            byte[] sizeByte = new byte[4];
+            sizeByte[0] = (byte)fileRead.ReadByte();
+            sizeByte[1] = (byte)fileRead.ReadByte();
+            sizeByte[2] = (byte)fileRead.ReadByte();
+            sizeByte[3] = (byte)fileRead.ReadByte();
+            int size = BitConverter.ToInt32(sizeByte);
+            fileRead.Seek(0, SeekOrigin.Begin);
+            var dictionary = new byte[size][];
+            var valueBytes = new byte[4];
 
-                        if (value == 0)
-                        {
-                            dictionary[i] = new byte[1];
-                            dictionary[i][0] = character;
-                            fileWrite.WriteByte(character);
-                        }
-                        else
-                        {
-                            dictionary[i] = new byte[dictionary[value - 1].Length + 1];
-                            for (int j = 0; j < dictionary[i].Length; j++)
-                            {
-                                dictionary[i][j] = j != dictionary[i].Length - 1 ? dictionary[value - 1][j] : character;
-                                fileWrite.WriteByte(dictionary[i][j]);
-                            }
-                        }
+            for (int i = 0; i < dictionary.GetLength(0); i++)
+            {
+                int bytesLength = (int)Math.Ceiling(Math.Log2(i + 2) / 8);
+                for (int j = 0; j < 4; j++)
+                {
+                    valueBytes[j] = j < bytesLength ? (byte)fileRead.ReadByte() : (byte)0;
+                }
+                int value = BitConverter.ToInt32(valueBytes);
+                byte character = (byte)fileRead.ReadByte();
+                if (value == 0)
+                {
+                    dictionary[i] = new byte[1];
+                    dictionary[i][0] = character;
+                    fileWrite.WriteByte(character);
+                }
+                else
+                {
+                    dictionary[i] = new byte[dictionary[value - 1].Length + 1];
+                    for (int j = 0; j < dictionary[i].Length; j++)
+                    {
+                        dictionary[i][j] = j != dictionary[i].Length - 1 ? dictionary[value - 1][j] : character;
+                        fileWrite.WriteByte(dictionary[i][j]);
                     }
                 }
             }
-        }
-
-        static private void WriteTrie(Trie cursor, byte valueByte, FileStream file)
-        {
-            int writeValue = cursor.HasParent() ? cursor.value : 0;
-            // byte[] bytes = BitConverter.GetBytes(writeValue);
-            byte[] bytes = GetBytes(writeValue);
-            file.WriteByte(BitConverter.GetBytes(bytes.Length)[0]);
-            file.Write(bytes);
-            file.WriteByte(valueByte);
-        }
-
-        static private byte[] GetBytes(int value)
-        {
-            byte[] bytes = BitConverter.GetBytes(value);
-            int length = 0;
-            for (int i = 3; i >= 0; i--)
-            {
-                if (bytes[i] != 0)
-                {
-                    length = i + 1;
-                    break;
-                }
-            }
-            Array.Resize(ref bytes, length);
-            return bytes;
         }
     }
 }
--- semester2/Task2/Task2.1/Trie.cs
+++ semester2/Task2/Task2.1/Trie.cs
@@ -1,43 +1,76 @@
 ﻿using System;
+using System.IO;
 using System.Collections;
 
 namespace Task2._1
 {
     class Trie
     {
-        public int value;
-        public static int counter = 1;
-        private Trie parent;
-        private Hashtable children;
+        private int counter;
+        private TrieElement head;
+        private TrieElement cursor;
 
         public Trie()
         {
-            children = new Hashtable();
+            counter = 1;
+            head = new TrieElement();
+            cursor = head;
         }
 
-        public Trie AddChild(byte keyByte)
+        public bool TryAdd(byte value)
         {
-            Trie newChild = new Trie();
-            newChild.parent = this;
-            newChild.value = counter;
+            if (cursor.HasChild(value))
+            {
+                cursor = cursor.GetChild(value);
+                return false;
+            }
+            cursor.AddChild(counter, value);
             counter++;
-            children.Add(keyByte, newChild);
-            return newChild;
+            return true;
         }
 
-        public bool HasChild(byte keyByte)
+        public void SaveTrieElement(byte value, FileStream file)
         {
-            return children.ContainsKey(keyByte);
+            int writeValue = cursor.HasParent() ? cursor.Value : 0;
+            byte[] bytes = BitConverter.GetBytes(writeValue);
+            Array.Resize(ref bytes, (int)(Math.Ceiling(Math.Log2(counter) / 8)));
+            file.Write(bytes);
+            file.WriteByte(value);
         }
 
-        public Trie GetChild(byte keyByte)
-        {
-            return (Trie)children[keyByte];
-        }
+        public void SaveTrieCount(FileStream file)
+            => file.Write(BitConverter.GetBytes(counter));
 
-        public bool HasParent()
+        public void ResetCursor()
+            => cursor = head;
+
+        private class TrieElement
         {
-            return parent != null;
+            public int Value { get; set; }
+            private TrieElement parent;
+            private Hashtable children;
+
+            public TrieElement()
+                => children = new Hashtable();
+
+            public void AddChild(int value, byte keyByte)
+            {
+                var newChild = new TrieElement();
+                newChild.parent = this;
+                newChild.Value = value;
+                children.Add(keyByte, newChild);
+            }
+
+            public bool HasChild(byte keyByte)
+                => children.ContainsKey(keyByte);
+
+            public TrieElement GetChild(byte keyByte)
+                => (TrieElement)children[keyByte];
+
+            public bool HasParent()
+                => parent != null;
         }
     }
+
+
 }

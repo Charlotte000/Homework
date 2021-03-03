33577b0e3485c9fb0ea15445fa66108ae146f04d
Charlotte000 <ramil.mifteev@yandex.ru>
2021-03-03 15:49:14 +0300

    init task3.1

---
+++ semester2/Task3/Task3.1/BTree.cs
@@ -0,0 +1,330 @@
+﻿using System;
+
+namespace Task3._1
+{
+    /// <summary>
+    /// B-tree data structure implementation  
+    /// </summary>
+    class BTree
+    {
+        private readonly int t;
+        private BNode head;
+
+        /// <param name="t">Degree</param>
+        public BTree(int t)
+        {
+            this.t = t;
+            head = new BNode(t, null);
+        }
+
+        /// <summary>
+        /// Adds value into the tree if isn't existed;
+        /// Otherwise changes it's value
+        /// </summary>
+        public void AddValue(int key, int value)
+        {
+            head.InsertValue(key, value, t);
+        }
+
+        /// <summary>
+        /// Get's value by its key
+        /// </summary>
+        /// <returns>Value</returns>
+        public int FindValue(int key)
+        {
+            return head.FindValue(key);
+        }
+
+        /// <summary>
+        /// Checks object availability
+        /// </summary>
+        public bool IsExists(int key)
+        {
+            return head.IsExists(key);
+        }
+
+        /// <summary>
+        /// Recursively draws entire tree
+        /// </summary>
+        public void PrintTree()
+        {
+            head.PrintValues();
+        }
+
+        /// <summary>
+        /// Node in the tree
+        /// </summary>
+        private class BNode
+        {
+            private bool isLeaf;
+            private Data[] values;
+            private BNode[] children;
+            private int realLen;
+            private BNode parent;
+
+            public BNode(int t, BNode parent)
+            {
+                values = new Data[2 * t - 1];
+                children = new BNode[2 * t];
+                isLeaf = true;
+                realLen = 0;
+                this.parent = parent;
+            }
+
+            /// <summary>
+            /// Prints node's information
+            /// </summary>
+            /// <param name="margin">Offset</param>
+            public void PrintValues(int margin = 0)
+            {
+                for (int j = 0; j < margin; j++) Console.Write(' ');
+                for (int i = 0; i < realLen; i++)
+                {
+                    Console.Write($"{values[i].Key}({values[i].Value}) ");
+                }
+                Console.WriteLine();
+
+                for (int i = 0; i <= realLen; i++)
+                {
+                    if (children[i] != null) children[i].PrintValues(margin + 2);
+                }
+            }
+
+            /// <summary>
+            /// Recursively adds values into itself or it's children
+            /// </summary>
+            /// <param name="degree">Tree's degree</param>
+            public void InsertValue(int key, int value, int degree)
+            {
+                if (isLeaf)
+                {
+                    if (realLen == 0)
+                    {
+                        values[0] = new Data(key, value);
+                        realLen++;
+                        return;
+                    }
+                    for (int i = 0; i < realLen; i++)
+                    {
+                        if (values[i].Key == key)
+                        {
+                            values[i].Value = value;
+                            return;
+                        }
+                        if (values[i].Key > key)
+                        {
+                            values[realLen] = new Data(0, 0);
+                            for (int j = realLen - 1; j >= i; j--)
+                            {
+                                values[j + 1].Key = values[j].Key;
+                                values[j + 1].Value = values[j].Value;
+                            }
+                            values[i].Value = value;
+                            realLen++;
+                            TrySplit(degree);
+                            return;
+                        }
+                    }
+                    values[realLen] = new Data(key, value);
+                    realLen++;
+                    TrySplit(degree);
+                    return;
+                }
+
+                if (key < values[0].Key)
+                {
+                    children[0].InsertValue(key, value, degree);
+                    return;
+                }
+                if (values[realLen - 1].Key < key)
+                {
+                    children[realLen].InsertValue(key, value, degree);
+                    return;
+                }
+                for (int i = 0; i < realLen; i++)
+                {
+                    if (values[i].Key < key && key < values[i + 1].Key)
+                    {
+                        children[i + 1].InsertValue(key, value, degree);
+                        return;
+                    }
+                }
+            }
+
+            /// <summary>
+            /// Recursively finds value 
+            /// </summary>
+            /// <param name="key"></param>
+            /// <returns></returns>
+            public int FindValue(int key)
+            {
+                if (isLeaf)
+                {
+                    for (int i = 0; i < realLen; i++)
+                    {
+                        if (values[i].Key == key)
+                        {
+                            return values[i].Value;
+                        }
+                    }
+                    return -1;
+                }
+                for (int i = 0; i < realLen; i++)
+                {
+                    if (values[i].Key == key)
+                    {
+                        return values[i].Value;
+                    }
+                    if (key < values[i].Key)
+                    {
+                        return children[i].FindValue(key);
+                    }
+                }
+                if (values[realLen - 1].Key < key)
+                {
+                    return children[realLen].FindValue(key);
+                }
+                return -1;
+            }
+
+            /// <summary>
+            /// Checks key-value availability
+            /// </summary>
+            public bool IsExists(int key)
+            {
+                if (isLeaf)
+                {
+                    for (int i = 0; i < realLen; i++)
+                    {
+                        if (values[i].Key == key)
+                        {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+                for (int i = 0; i < realLen; i++)
+                {
+                    if (values[i].Key == key)
+                    {
+                        return true;
+                    }
+                    if (key < values[i].Key)
+                    {
+                        return children[i].IsExists(key);
+                    }
+                }
+                if (values[realLen - 1].Key < key)
+                {
+                    return children[realLen].IsExists(key);
+                }
+                return false;
+            }
+
+            /// <summary>
+            /// Splits node in case of overflow with children
+            /// </summary>
+            /// <param name="degree">Tree's degree</param>
+            private void TrySplit(int degree)
+            {
+                if (realLen == 2 * degree - 1)
+                {
+                    isLeaf = false;
+                    BNode left = new BNode(degree, this);
+                    for (int i = 0; i < degree; i++)
+                    {
+                        if (i != degree - 1)
+                        {
+                            left.values[i] = values[i].Copy();
+                        }
+                        if (children[i] != null)
+                        {
+                            children[i].parent = left;
+                            left.isLeaf = false;
+                        }
+                        left.children[i] = children[i];
+                    }
+                    left.realLen = degree - 1;
+                    BNode right = new BNode(degree, this);
+                    for (int i = 0; i < degree; i++)
+                    {
+                        if (i != degree - 1)
+                        {
+                            right.values[i] = values[degree + i].Copy();
+                        }
+                        if (children[degree + i] != null)
+                        {
+                            children[degree + i].parent = right;
+                            right.isLeaf = false;
+                        }
+                        right.children[i] = children[degree + i];
+                    }
+                    right.realLen = degree - 1;
+                    Data middle = values[(2 * degree - 1) / 2].Copy();
+
+                    if (parent == null)
+                    {
+                        Array.Clear(values, 0, values.Length);
+                        Array.Clear(children, 0, children.Length);
+                        realLen = 1;
+                        values[0] = middle;
+                        children[0] = left;
+                        children[1] = right;
+                        return;
+                    }
+                    left.parent = parent;
+                    right.parent = parent;
+                    for (int i = 0; i < parent.realLen; i++)
+                    {
+                        if (parent.values[i].Key > middle.Key)
+                        {
+                            for (int j = parent.realLen - 1; j >= i; j--)
+                            {
+                                parent.values[j + 1] = parent.values[j].Copy();
+                            }
+                            parent.values[i].Key = middle.Key;
+                            parent.values[i].Value = middle.Value;
+
+                            for (int j = parent.realLen + 1; j > i; j--)
+                            {
+                                parent.children[j] = parent.children[j - 1];
+                            }
+                            parent.children[i] = left;
+                            parent.children[i + 1] = right;
+                            parent.realLen++;
+                            parent.TrySplit(degree);
+                            return;
+                        }
+                    }
+                    parent.values[parent.realLen] = middle;
+                    parent.children[parent.realLen] = left;
+                    parent.children[parent.realLen + 1] = right;
+                    parent.realLen++;
+                    parent.TrySplit(degree);
+                    return;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Key-value structure
+        /// </summary>
+        private class Data
+        {
+            public Data(int key, int value)
+            {
+                Key = key;
+                Value = value;
+            }
+
+            public Data Copy()
+            {
+                return new Data(Key, Value);
+            }
+
+            public int Key { get; set; }
+
+            public int Value { get; set; }
+        }
+    }
+}
---
+++ semester2/Task3/Task3.1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BTree tree = new BTree(3);
+            tree.AddValue(1, 0);
+            tree.AddValue(2, 0);
+            tree.AddValue(20, 0);
+            tree.AddValue(21, 0);
+            tree.AddValue(22, 0);
+
+            tree.AddValue(30, 0);
+            tree.AddValue(31, 0);
+            tree.AddValue(32, 0);
+
+            tree.AddValue(3, 0);
+            tree.AddValue(4, 0);
+            tree.AddValue(5, 0);
+
+            tree.AddValue(6, 0);
+            tree.AddValue(7, 0);
+            tree.AddValue(8, 6);
+
+            tree.AddValue(23, 3);
+            tree.AddValue(24, 0);
+            tree.AddValue(25, 0);
+            tree.AddValue(26, 0);
+            tree.AddValue(27, 1);
+            tree.AddValue(28, 0);
+
+            // tree.AddValue(27, 2);
+
+            tree.PrintTree();
+        }
+    }
+}
